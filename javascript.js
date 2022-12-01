1e48d3448277f088674794a45fe1c4cdb32556a5
Luca Di Matteo <dimatteoluca1995@gmail.com>
2022-12-01 22:55:00 +0100

    Add mobile mode

--- javascript.js
+++ javascript.js
@@ -5,6 +5,10 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+let touchStart = false;
+document.body.ontouchstart = () => (touchStart = true);
+document.body.ontouchend = () => (touchStart = false);
+
 function createGrid(n) {
     // const containerSide = container.clientWidth;
     container.setAttribute('style', `grid-template-rows: repeat(${n}, ${100/n}%);`+
@@ -15,19 +19,39 @@ function createGrid(n) {
             const temp = document.createElement('div');
             temp.id = `${i}-${j}`;
             temp.classList.add('grid-item');
-            if ((i+j)%2 == 1) temp.setAttribute('style', 'background-color: lightgrey');
+            if ((i+j)%2 == 1)
+                temp.setAttribute('style', 'background-color: lightgrey');
             // temp.setAttribute('style', `width: ${divSide}px; height: ${divSide}px;`);
-            temp.addEventListener('mouseover', changeColor);
-            temp.addEventListener('mousedown', changeColor);
+            if (isMobileDevice()) {
+                temp.addEventListener('touchstart', changeColor);
+                temp.addEventListener('touchmove', changeColor);
+            }
+            else {
+                temp.addEventListener('mouseover', changeColor);
+                temp.addEventListener('mousedown', changeColor);
+            }
             container.appendChild(temp);
         }
     }
 }
 
 function changeColor(e) {
-    if (e.type === 'mouseover' && !mouseDown) return;
-    e.target.style.backgroundColor = DEFAULT_COLOR;
+    if (isMobileDevice()) {
+        if (e.type === 'touchmove' && !touchStart) return;
+        e.target.style.backgroundColor = DEFAULT_COLOR;
+    }
+    else {
+        if (e.type === 'mouseover' && !mouseDown) return;
+        e.target.style.backgroundColor = DEFAULT_COLOR;
+    }
 
+}
+
+function isMobileDevice() {
+    if (navigator.userAgent.toLowerCase().match(/mobile/i))
+        return true;
+    else
+        return false;
 }
 
 const container = document.querySelector('#container');
