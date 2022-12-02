bf3d4680fe4c3d6cbda825cdcea9260fd800971e
Luca Di Matteo <dimatteoluca1995@gmail.com>
2022-12-02 14:52:49 +0100

    Add shading mode + Reorganize js

--- javascript.js
+++ javascript.js
@@ -13,6 +13,43 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+
+function isMobileDevice() {
+    if (navigator.userAgent.toLowerCase().match(/mobile/i))
+        return true;
+    else
+        return false;
+}
+
+function goRainbow() {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    return `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
+function goShading(backgroundColor) {
+    const rgb = backgroundColor.split('(')[1].split(')')[0].split(',');
+    const r = rgb[0].trim();
+    const g = rgb[1].trim();
+    const b = rgb[2].trim();
+    return `rgb(${Number(r)-25}, ${Number(g)-25}, ${Number(b)-25})`;
+}
+
+function changeColor(e) {
+    if (!isMobileDevice())
+        if (e.type === 'mouseover' && !mouseDown) return;
+
+    if (modeSelect.value == "color")
+        color = DEFAULT_COLOR;
+    else if (modeSelect.value == "rainbow")
+        color = goRainbow();
+    else if (modeSelect.value == "shading") {
+        color = goShading(window.getComputedStyle(e.target).getPropertyValue('background-color'));
+    }
+    e.target.style.backgroundColor = color;
+}
+
 function createGrid(n) {
     // const containerSide = container.clientWidth;
     container.setAttribute('style', `grid-template-rows: repeat(${n}, ${100/n}%);`+
@@ -37,32 +74,6 @@ function createGrid(n) {
     }
 }
 
-function changeColor(e) {
-    if (modeSelect.value == "color")
-        color = DEFAULT_COLOR;
-    else if (modeSelect.value == "rainbow") {
-        const randomR = Math.floor(Math.random() * 256);
-        const randomG = Math.floor(Math.random() * 256);
-        const randomB = Math.floor(Math.random() * 256);
-        color = `rgb(${randomR}, ${randomG}, ${randomB})`;
-    }
-    if (isMobileDevice()) {
-        e.target.style.backgroundColor = color;
-    }
-    else {
-        if (e.type === 'mouseover' && !mouseDown) return;
-        e.target.style.backgroundColor = color;
-    }
-
-}
-
-function isMobileDevice() {
-    if (navigator.userAgent.toLowerCase().match(/mobile/i))
-        return true;
-    else
-        return false;
-}
-
 function empty(element) {
     while(element.firstElementChild) {
         element.firstElementChild.remove();
@@ -85,6 +96,7 @@ clearButton.addEventListener('click', () => {
     empty(container);
     createGrid(sizeSelect.value);
 });
+
 
 if (isMobileDevice()) {
     sizeSelect.removeChild(value64);
